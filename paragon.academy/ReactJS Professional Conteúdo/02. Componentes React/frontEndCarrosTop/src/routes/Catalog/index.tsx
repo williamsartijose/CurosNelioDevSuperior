9437f2339f98a94c9dc8cd188087013fa05e5371
unknown <williamsartijose@hotmail.com>
2023-10-20 14:41:14 +0100

    Carros Top estruturando página de catálogo

--- paragon.academy/ReactJS Professional Conteúdo/02. Componentes React/frontEndCarrosTop/src/routes/Catalog/index.tsx
+++ paragon.academy/ReactJS Professional Conteúdo/02. Componentes React/frontEndCarrosTop/src/routes/Catalog/index.tsx
@@ -1,7 +1,24 @@
+
+import './styles.css'
 import Header from "../../components/Header";
+import SearchBar from '../../components/Header/SearchBar';
 
 export default function Catalog () {
     return (
-        <Header/>
+        <>
+         <Header/>
+
+         <main className="ct-catalog-main">
+            <section id="ct-catalog-section">
+             <div className="ct-search-bar-container">
+             <SearchBar/>
+             </div>
+             <div className="ct-catalog-cards-container">
+
+             </div>
+            </section>
+         </main>
+        </>
+       
     );
 }
