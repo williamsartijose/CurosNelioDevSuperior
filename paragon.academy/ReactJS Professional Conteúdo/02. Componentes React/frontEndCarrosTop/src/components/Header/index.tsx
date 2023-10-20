4c78b499d4ed307b7ebba5c22857063401145398
unknown <williamsartijose@hotmail.com>
2023-10-20 10:32:57 +0100

    Carros Top exercício resolvido, começando

---
+++ paragon.academy/ReactJS Professional Conteúdo/02. Componentes React/frontEndCarrosTop/src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import './styles.css'
+export default function Header () {
+    return (
+        <>
+         <header className="dsc-header">
+      <nav className="dsc-container">
+        <h1>Carros Top</h1>
+        <div className="dsc-navbar-right">
+          <div className="dsc-menu-items-container">
+          </div>
+       
+        </div>
+      </nav>
+    </header>
+        </>
+    );
+}
