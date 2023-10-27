de112f34e712931bd5a2a64d362976ef2aa63dd6
unknown <williamsartijose@hotmail.com>
2023-10-27 15:22:14 +0100

    Rota para não encontrado

--- paragon.academy/ReactJS Professional Conteúdo/03. Props, Rotas/tutorial-react-router/src/main.tsx
+++ paragon.academy/ReactJS Professional Conteúdo/03. Props, Rotas/tutorial-react-router/src/main.tsx
@@ -4,14 +4,17 @@ import App from "./App.tsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Expenses from "./routes/Expenses/index.tsx";
 import Invoices from "./routes/Invoices/index.tsx";
+import NotFound from "./routes/NotFound/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<App />}>
-      <Route path="expenses" element={<Expenses />} />
-      <Route path="invoices" element={<Invoices />} />
-    </Route>
-  </Routes>
-</BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />} />
+        <Route path="*" element={<NotFound />} 
+        />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
