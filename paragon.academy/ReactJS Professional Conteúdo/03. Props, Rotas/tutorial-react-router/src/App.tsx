b61970e00fa0001b6d97db3b842c7e206fd778b9
unknown <williamsartijose@hotmail.com>
2023-10-27 14:37:27 +0100

    Adicionando primeiros links

--- paragon.academy/ReactJS Professional Conteúdo/03. Props, Rotas/tutorial-react-router/src/App.tsx
+++ paragon.academy/ReactJS Professional Conteúdo/03. Props, Rotas/tutorial-react-router/src/App.tsx
@@ -1,9 +1,16 @@
-
-import './App.css'
+import { Link } from "react-router-dom";
+import "./App.css";
 
 export default function App() {
-
   return (
-    <h1>Bookkeeper!</h1>
-  )
+    <nav
+      style={{
+        borderBottom: "solid 1px",
+        paddingBottom: "1rem",
+      }}
+    >
+      <Link to="/invoices">Invoices</Link> |{" "}
+      <Link to="/expenses">Expenses</Link>
+    </nav>
+  );
 }
