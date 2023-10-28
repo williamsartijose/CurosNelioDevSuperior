aa2c27db6b1a5dd7b6d9d6c1883e9944ed4c4e22
unknown <williamsartijose@hotmail.com>
2023-10-28 23:29:43 +0100

    Acessando parâmetros de URL

--- paragon.academy/ReactJS Professional Conteúdo/03. Props, Rotas/tutorial-react-router/src/main.tsx
+++ paragon.academy/ReactJS Professional Conteúdo/03. Props, Rotas/tutorial-react-router/src/main.tsx
@@ -1,20 +1,23 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Expenses from "./routes/Expenses/index.tsx";
-import Invoices from "./routes/Invoices/index.tsx";
-import NotFound from "./routes/NotFound/index.tsx";
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import App from './App';
+import Expenses from './routes/Expenses';
+import Invoices from './routes/Invoices';
+import Invoice from './routes/Invoices/Invoice';
+import NotFound from './routes/NotFound';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="expenses" element={<Expenses />} />
-        <Route path="invoices" element={<Invoices />} />
+        <Route path="invoices" element={<Invoices />}>
+          <Route path=":invoiceId" element={<Invoice />} />
+        </Route>
         <Route path="*" element={<NotFound />} 
         />
       </Route>
     </Routes>
   </BrowserRouter>
-);
+)
