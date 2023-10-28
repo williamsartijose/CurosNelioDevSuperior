8a9547b7fe1628ddf88b401cc3917e685a78c760
unknown <williamsartijose@hotmail.com>
2023-10-28 23:45:27 +0100

    Rotas index

--- paragon.academy/ReactJS Professional Conteúdo/03. Props, Rotas/tutorial-react-router/src/main.tsx
+++ paragon.academy/ReactJS Professional Conteúdo/03. Props, Rotas/tutorial-react-router/src/main.tsx
@@ -1,23 +1,26 @@
 import React from "react";
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import App from './App';
-import Expenses from './routes/Expenses';
-import Invoices from './routes/Invoices';
-import Invoice from './routes/Invoices/Invoice';
-import NotFound from './routes/NotFound';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App";
+import Expenses from "./routes/Expenses";
+import Invoices from "./routes/Invoices";
+import Invoice from "./routes/Invoices/Invoice";
+import NotFound from "./routes/NotFound";
+import InvoicesIndex from "./routes/Invoices/InvoicesIndex";
+import Welcome from "./routes/Welcome";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
+      <Route index element={<Welcome/> }/>
         <Route path="expenses" element={<Expenses />} />
         <Route path="invoices" element={<Invoices />}>
+          <Route index element={<InvoicesIndex/> }/>
           <Route path=":invoiceId" element={<Invoice />} />
         </Route>
-        <Route path="*" element={<NotFound />} 
-        />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   </BrowserRouter>
-)
+);
