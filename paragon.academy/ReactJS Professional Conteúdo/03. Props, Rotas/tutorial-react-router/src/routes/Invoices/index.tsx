3dc7c469fb450c74552612922a32ac921de235bc
unknown <williamsartijose@hotmail.com>
2023-11-22 09:39:44 +0000

    Links ativos com NavLink

--- paragon.academy/ReactJS Professional Conteúdo/03. Props, Rotas/tutorial-react-router/src/routes/Invoices/index.tsx
+++ paragon.academy/ReactJS Professional Conteúdo/03. Props, Rotas/tutorial-react-router/src/routes/Invoices/index.tsx
@@ -1,5 +1,6 @@
-import { Link, Outlet } from "react-router-dom";
+import './styles.css';
 import { getInvoices } from "../../data";
+import { NavLink, Outlet } from "react-router-dom";
 
 export default function Invoices() {
   const invoices = getInvoices();
@@ -13,16 +14,16 @@ export default function Invoices() {
         }}
       >
         {invoices.map((invoice) => (
-          <Link
-            style={{ display: "block", margin: "1rem 0" }}
-            to={`/invoices/${invoice.number}`}
-            key={invoice.number}
-          >
-            {invoice.name}
-          </Link>
+                <NavLink
+                className={({ isActive }) => isActive ? "dblock nav-red" : "dblock nav-blue"}
+                to={`/invoices/${invoice.number}`}
+                key={invoice.number}
+            >
+                {invoice.name}
+            </NavLink>
         ))}
-      </nav>
-      <Outlet />
+    </nav>
+    <Outlet />
     </div>
   );
 }
