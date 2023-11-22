a6205e952472e5bfae51976c9d48c3142f9e48dc
unknown <williamsartijose@hotmail.com>
2023-11-22 23:06:08 +0000

    De volta ao DSCommerce, adicionando rotas reactrouterDOM

---
+++ paragon.academy/ReactJS Professional Conteúdo/03. Props, Rotas/dscommerce-frontend/src/routes/ClientHome/ProductDetails/index.tsx
@@ -0,0 +1,42 @@
+import "./styles.css";
+import ButtonInverse from "../../../components/ButtonInverse";
+import ButtonPrimary from "../../../components/ButtonPrimary";
+import ProductDetailsCard from "../../../components/ProductDetailsCard";
+import { ProductDTO } from "../../../models/product";
+
+const product: ProductDTO = {
+  id: 2,
+  name: "Smart TV",
+  description: "40'' LED Smart TV with 1080p resolution and WiFi connection.",
+  imgUrl:
+    "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg",
+  price: 2500.99,
+  categories: [
+    {
+      id: 2,
+      name: "Eletrônicos",
+    },
+    {
+      id: 3,
+      name: "Computadores",
+    },
+    {
+      id: 4,
+      name: "Importados",
+    },
+  ],
+};
+
+export default function ProductDetails() {
+  return (
+    <main>
+      <section id="product-details-section" className="dsc-container">
+        <ProductDetailsCard product={product} />
+        <div className="dsc-btn-page-container">
+          <ButtonPrimary text="Comprar" />
+          <ButtonInverse text="Inicio" />
+        </div>
+      </section>
+    </main>
+  );
+}
