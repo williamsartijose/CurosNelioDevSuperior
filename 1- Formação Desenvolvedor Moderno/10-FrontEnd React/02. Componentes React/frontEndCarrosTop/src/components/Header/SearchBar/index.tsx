66eb818f739dda1e0d49acd10545d1c1b026b8a7
unknown <williamsartijose@hotmail.com>
2024-01-30 22:07:31 +0000

    Adicionando alterações nos componentes React e no conteúdo do curso

--- 1- Formação Desenvolvedor Moderno/10-FrontEnd React/02. Componentes React/frontEndCarrosTop/src/components/Header/SearchBar/index.tsx
+++ 1- Formação Desenvolvedor Moderno/10-FrontEnd React/02. Componentes React/frontEndCarrosTop/src/components/Header/SearchBar/index.tsx
@@ -1,9 +1,9 @@
 import './styles.css'
 export default function SearchBar () {
     return (
-        <form>
-         <input type="text" placeholder="Digite sua busca" />
-         <button type="submit">Buscar</button>
+        <form className="ct-search-bar">
+         <input className="ct-form-control"  type="text" placeholder="Digite sua busca" />
+         <button className="ct-search-bar-submit" type="submit">Buscar</button>
         </form>
     );
 }
