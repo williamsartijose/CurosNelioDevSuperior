32ade03534a84b4d765798fcc83054e054c97459
unknown <williamsartijose@hotmail.com>
2024-02-08 10:33:26 +0000

    05-05 Layout da tela de carrinho

--- 1- Formação Desenvolvedor Moderno/10-FrontEnd React/04. Hooks UseStateUseEffectRequisicao/dscommerce-frontend/src/App.tsx
+++ 1- Formação Desenvolvedor Moderno/10-FrontEnd React/04. Hooks UseStateUseEffectRequisicao/dscommerce-frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import ClientHome from "./routes/ClientHome";
 import Catalog from "./routes/ClientHome/Catalog";
 import ProductDetails from "./routes/ClientHome/ProductDetails";
+import Cart from "./routes/ClientHome/Cart";
 
 
 export default function App() {
@@ -10,8 +11,10 @@ export default function App() {
     <Routes>
       <Route path="/" element={<ClientHome />}>
       <Route index element={<Catalog />}/>
-        <Route path="catalog" element={<ProductDetails />} />
+        <Route path="catalog" element={<Catalog />} />
         <Route path="product-details/:productId" element={<ProductDetails />} />
+        <Route path="cart" element={<Cart />} />
+
       </Route>
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
