24ab5d0e2f8393a691202c8c22e7582a56f20681
unknown <williamsartijose@hotmail.com>
2024-02-09 18:57:43 +0000

    05-15 Incrementar a quantidade de um item do carrinho

--- 1- Formação Desenvolvedor Moderno/10-FrontEnd React/04. Hooks UseStateUseEffectRequisicao/dscommerce-frontend/src/Services/cart-service.ts
+++ 1- Formação Desenvolvedor Moderno/10-FrontEnd React/04. Hooks UseStateUseEffectRequisicao/dscommerce-frontend/src/Services/cart-service.ts
@@ -24,3 +24,18 @@ export function addProduct(product: ProductDTO){
 export  function clearCart() {
     cartRepository.clear();
 }
+
+
+
+export function increaseItem(productId: number) {
+    const cart =  cartRepository.get(); 
+    const item = cart.items.find(x => x.productId == productId);
+    if (item) {
+
+        item.quantity ++;
+        cartRepository.save(cart);
+    }
+
+
+
+}
