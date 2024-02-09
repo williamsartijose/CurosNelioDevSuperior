68f911adeb494db306053a5e08ebe0563b31066e
unknown <williamsartijose@hotmail.com>
2024-02-09 19:04:53 +0000

    05-16 Decrementar a quantidade de um item do carrinho

--- 1- Formação Desenvolvedor Moderno/10-FrontEnd React/04. Hooks UseStateUseEffectRequisicao/dscommerce-frontend/src/routes/ClientHome/Cart/index.tsx
+++ 1- Formação Desenvolvedor Moderno/10-FrontEnd React/04. Hooks UseStateUseEffectRequisicao/dscommerce-frontend/src/routes/ClientHome/Cart/index.tsx
@@ -18,6 +18,12 @@ export default function Cart() {
   setCart(cartService.getCart());
   }
 
+  function handleDecreseItem(productId:number) {
+    cartService.decreaseItem(productId);
+    setCart(cartService.getCart());
+    }
+  
+
   return (
     <main>
       <section id="cart-container-section" className="dsc-container">
@@ -38,7 +44,7 @@ export default function Cart() {
                   <div className="dsc-cart-item-description">
                     <h3>{item.name}</h3>
                     <div className="dsc-cart-item-quantity-container">
-                      <div className="dsc-cart-item-quantity-btn">-</div>
+                      <div onClick={() => handleDecreseItem(item.productId)} className="dsc-cart-item-quantity-btn">-</div>
                       <p>{item.quantity}</p>
                       <div onClick={() => handleIncreaseItem(item.productId)} className="dsc-cart-item-quantity-btn">+</div>
                     </div>
