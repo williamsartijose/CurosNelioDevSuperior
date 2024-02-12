77896804f2d2d55e3f982ec5fa15b66af9612e47
unknown <williamsartijose@hotmail.com>
2024-02-12 22:42:00 +0000

    06-12 Buscando os dados do localStorage

--- 1- Formação Desenvolvedor Moderno/10-FrontEnd React/04. Hooks UseStateUseEffectRequisicao/dscommerce-frontend/src/components/CartIcon/index.tsx
+++ 1- Formação Desenvolvedor Moderno/10-FrontEnd React/04. Hooks UseStateUseEffectRequisicao/dscommerce-frontend/src/components/CartIcon/index.tsx
@@ -1,11 +1,14 @@
 import "./styles.css";
 import cartIcon from "../../assets/cart.svg";
+import * as cartService from "../../Services/cart-service"
+import { useState } from "react";
 
 export default function CartIcon() {
+    const [cart, setCart] = useState(cartService.getCart());
   return (
     <>
       <img src={cartIcon} alt="Carrinho de compras" />
-        <div className="dsc-cart-count">2</div>
+        <div className="dsc-cart-count">{cart.items.length}</div>
       
     </>
   );
